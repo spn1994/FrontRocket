3da415b6ded913e47d5998b957c954e9be25458a
Sergio pedrosa Nahirny <spn1994@gmail.com>
2023-08-30 14:57:31 -0300

    Update index.jsx

--- src/pages/Signin/index.jsx
+++ src/pages/Signin/index.jsx
@@ -1,52 +1,49 @@
 import { useState } from 'react';
 import { FiMail, FiLock } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
+import { Input } from '../../components/Input';
+import { Button } from '../../components/Button';
 
-import { Input } from '../../components/input';
-import { Button } from '../../components/button';
-
-import { useAuth } from '../../hooks/auth.js'
-
+import { useAuth } from '../../hooks/auth';
 import { Container, Form, Background } from './styles';
 
-export function SignIn(){
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+export function SignIn() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-  const { signIn } = useAuth();
-  //console.log("meu contexto", signIn)
+    const { signIn } = useAuth();
 
-  function handleSignIn(){
-    signIn({ email, password });
-  }
+    function handleSignIn() {
+        signIn({ email, password });
+    }
+    return (
+        <Container>
+            <Form>
+                <h1>Rocket Notes</h1>
+                <p>Aplicação para salvar e gerenciar seus links uteis.</p>
 
-  return (
-    
-    <Container>
-      <Form>
-        <h1>Rocket Notes</h1>
-        <p>Aplicação para salvar e gerenciar seus links úteis</p>
+                <h2>Faça seu login</h2>
+                <Input
+                    placeholder="E-mail"
+                    type="text"
+                    icon={FiMail}
+                    onChange={e => setEmail(e.target.value)}
+                />
+                <Input
+                    placeholder="Senha"
+                    type="password"
+                    icon={FiLock}
+                    onChange={e => setPassword(e.target.value)}
+                />
 
-        <h2>Faça seu login</h2>
+                <Button
+                    title="Entrar"
+                    onClick={handleSignIn}
+                ></Button>
 
-        <Input
-          placeholder="E-mail"
-          type="text"
-          icon={FiMail}
-          onChange={e => setEmail(e.target.value)}
-        />
-        <Input
-          placeholder="senha"
-          type="password"
-          icon={FiLock}
-          onChange={e => setPassword(e.target.value)}
-        />
-        <Button title="Entrar" onClick={handleSignIn}/>
-        <Link to="/register" >
-          Criar conta
-        </Link>
-      </Form>
-      <Background/>
-    </Container>
-  )
+                <Link to="/register">Criar conta</Link>
+            </Form>
+            <Background />
+        </Container>
+    );
 }
